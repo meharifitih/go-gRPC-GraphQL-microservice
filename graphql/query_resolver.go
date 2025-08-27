5574f681d14c0e5b885fdd322928a51c30399f31
Mehari Fitihamlak <meharifitih@gmail.com>
2025-08-27 14:36:50 +0300

    graphql structure for queries and mutations

--- graphql/query_resolver.go
+++ graphql/query_resolver.go
@@ -1,1 +1,12 @@
 package main
+
+import "context"
+
+type queryResolver struct {
+	server *Server
+}
+
+func (q *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput,
+	id *string) ([]*Account, error)
+func (q *queryResolver) Products(ctx context.Context, pagination *PaginationInput,
+	query *string, id *string) ([]*Product, error)
