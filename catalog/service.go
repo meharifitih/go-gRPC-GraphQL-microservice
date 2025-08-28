528e93bc79d8d190303e014a001f46d13164d67b
Mehari Fitihamlak <meharifitih@gmail.com>
2025-08-28 22:39:07 +0300

    generate grpc and catalog service and server implemenation

--- catalog/service.go
+++ catalog/service.go
@@ -1,6 +1,11 @@
 package catalog
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	"github.com/segmentio/ksuid"
+)
 
 type Service interface {
 	PostProduct(ctx context.Context, name, description string, price float64) (*Product, error)
@@ -25,22 +30,41 @@ func NewCatalogService(r Repository) Service {
 	return &catalogService{repository: r}
 }
 
-func (s *catalogService) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error){
+func (s *catalogService) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
+	p := &Product{
+		ID:          ksuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       fmt.Sprintf("%v", price),
+	}
 
+	if err := s.repository.PutProduct(ctx, *p); err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
 
-func (s *catalogService) GetProduct(ctx context.Context, id string) (*Product, error){
-
+func (s *catalogService) GetProduct(ctx context.Context, id string) (*Product, error) {
+	return s.repository.GetProductByID(ctx, id)
 }
 
 func (s *catalogService) GetProducts(ctx context.Context, skip, take uint64) ([]Product, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
 
+	return s.repository.ListProducts(ctx, skip, take)
 }
 
 func (s *catalogService) GetProductsByIDs(ctx context.Context, ids []string) ([]Product, error) {
-
+	return s.repository.ListProductsWithIDs(ctx, ids)
 }
 
-func (s *catalogService) SearchProducts(ctx context.Context, query string, skip, take uint64) ([]Product, error){
-	
+func (s *catalogService) SearchProducts(ctx context.Context, query string, skip, take uint64) ([]Product, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+
+	return s.repository.SearchProducts(ctx, query, skip, take)
 }
