2ebe06d8aa20f56c74c28c2cfa385f50b6961440
Mehari Fitihamlak <meharifitih@gmail.com>
2025-09-01 19:58:49 +0300

    order service protofile and create order api implementation

--- order/cmd/order/main.go
+++ order/cmd/order/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/meharifitih/go-grpc-graphql-microservice/order"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL       string `envconfig:"DATABASE_URL"`
+	AccountServiceURL string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogServiceURL string `envconfig:"CATALOG_SERVICE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r order.Repository
+	retry.ForeverSleep(2*time.Second, func(i int) (err error) {
+		r, err = order.NewPostgresRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		return
+	})
+
+	defer r.Close()
+
+	log.Println("Listening on port 8080...")
+	s := order.NewOrderService(r)
+	log.Fatal(order.ListenGRPC(s, cfg.AccountServiceURL, cfg.CatalogServiceURL, 80808))
+}
--- order/server.go
+++ order/server.go
@@ -1,1 +1,119 @@
 package order
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/meharifitih/go-grpc-graphql-microservice/account"
+	"github.com/meharifitih/go-grpc-graphql-microservice/catalog"
+	pb "github.com/meharifitih/go-grpc-graphql-microservice/order/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	service       Service
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	pb.OrderServiceServer
+}
+
+func ListenGRPC(s Service, accountURL, catalogURL string, port int) error {
+	accountClient, err := account.NewClient(accountURL)
+	if err != nil {
+		return err
+	}
+
+	catalogClient, err := catalog.NewClient(catalogURL)
+	if err != nil {
+		accountClient.Close()
+		return err
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
+		return err
+	}
+
+	serv := grpc.NewServer()
+	pb.RegisterOrderServiceServer(serv, &grpcServer{
+		service:       s,
+		accountClient: accountClient,
+		catalogClient: catalogClient,
+	})
+
+	reflection.Register(serv)
+
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostOrder(ctx context.Context, r *pb.PostOrderRequest) (*pb.PostOrderResponse, error) {
+	_, err := s.accountClient.GetAccount(ctx, r.AccountId)
+	if err != nil {
+		log.Println("error getting account: ", err)
+		return nil, err
+	}
+
+	productIDs := []string{}
+
+	orderedProducts, err := s.catalogClient.GetProducts(ctx, 0, 0, productIDs, "")
+	if err != nil {
+		log.Println("error getting products ", err)
+		return nil, errors.New("products not found")
+	}
+
+	products := []OrderedProduct{}
+	for _, p := range orderedProducts {
+		product := OrderedProduct{
+			ID:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Quantity:    0,
+		}
+
+		for _, rp := range r.Products {
+			if rp.ProductId == p.ID {
+				product.Quantity = int64(rp.Quantity)
+				break
+			}
+		}
+		if product.Quantity != 0 {
+			products = append(products, product)
+		}
+	}
+
+	order, err := s.service.PostOrder(ctx, r.AccountId, products)
+	if err != nil {
+		log.Println("error posting order: ", err)
+		return nil, errors.New("could not post error")
+	}
+
+	orderProto := pb.Order{
+		Id:         order.ID,
+		AccountId:  order.AccountID,
+		TotalPrice: order.TotalPrice,
+		Products:   []*pb.Order_OrderProduct{},
+	}
+
+	orderProto.CreatedAt, _ = order.CreatedAt.MarshalBinary()
+
+	for _, p := range order.Products {
+		orderProto.Products = append(orderProto.Products, &pb.Order_OrderProduct{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Quantity:    uint32(p.Quantity),
+		})
+	}
+
+	return &pb.PostOrderResponse{Order: &orderProto}, nil
+}
+
+func (s *grpcServer) GetOrdersForAccount(context.Context, *pb.GetOrdersForAccountRequest) (*pb.GetOrdersForAccountResponse, error)
