3ed9fc50e40b43769dd1a96a0dc68ba9f44b2419
Mehari Fitihamlak <meharifitih@gmail.com>
2025-09-01 12:34:55 +0300

    service and repository implemenations for order microservice

---
+++ order/service.go
@@ -0,0 +1,62 @@
+package order
+
+import (
+	"context"
+	"time"
+
+	"github.com/segmentio/ksuid"
+)
+
+type Service interface {
+	PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error)
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
+}
+
+type Order struct {
+	ID         string
+	CreatedAt  time.Time
+	TotalPrice float64
+	AccountID  string
+	Products   []OrderedProduct
+}
+
+type OrderedProduct struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int64
+}
+
+type orderService struct {
+	repository Repository
+}
+
+func NewOrderService(r Repository) Service {
+	return &orderService{repository: r}
+}
+
+func (s *orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error) {
+	o := &Order{
+		ID:        ksuid.New().String(),
+		CreatedAt: time.Now().UTC(),
+		AccountID: accountID,
+		Products:  products,
+	}
+
+	o.TotalPrice = 0.0
+	for _, p := range products {
+		o.TotalPrice = p.Price * float64(p.Quantity)
+	}
+
+	err := s.repository.PutOrder(ctx, *o)
+	if err != nil {
+		return nil, err
+	}
+
+	return o, nil
+}
+
+func (s *orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	return s.GetOrdersForAccount(ctx, accountID)
+}
